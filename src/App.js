042024efbfacec8437a7d4c1c40857d448c72f11
Moiseis Cruz <moizzito@gmail.com>
2023-11-07 20:56:03 -0300

    Add Header.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Header } from './components/header';
 import { Main } from './components/main/main';
 import { ThemeProvider } from './themes-toggler';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <ThemeProvider>
+        <Header />
         <Main />
       </ThemeProvider>
     </>
