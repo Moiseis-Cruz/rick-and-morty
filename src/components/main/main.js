bd5534d50a81c166415722c8d880b6674761e191
Moiseis Cruz <moizzito@gmail.com>
2023-11-07 20:19:17 -0300

    I imported state and effect hooks.

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,10 +1,21 @@
+import React, { useState, useEffect } from "react";
+
 async function getDatos() {
     const response = await fetch("https://rickandmortyapi.com/api/character")
     const datos = await response.json()
     return datos.results
 }
 
+console.log(await getDatos());
+
 export const Main = () => {
+
+    const [ character, setCharacter ] = useState({avatar: []})
+
+    useEffect(() => {
+
+    })
+
     return(
         <h1>Teste</h1>
     )
