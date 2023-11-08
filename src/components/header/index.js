95eaf9972c62303c3e8648292b0fce6771efc939
Moiseis Cruz <moizzito@gmail.com>
2023-11-07 21:19:22 -0300

    Add <Button>

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,14 @@
+import { Button } from "../button"
 import ImgLogo from "../imgs/rick-and-morty.png"
 
 export const Header = () => {
     return(
         <header>
             <img src={ImgLogo} />
+
+            <Button>
+                Mudar Tema
+            </Button>
         </header>
     )
 }
