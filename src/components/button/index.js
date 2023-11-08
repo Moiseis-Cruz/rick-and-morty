c21b1abcab049651653235ae2bba09ea4cc13ef9
Moiseis Cruz <moizzito@gmail.com>
2023-11-08 12:41:47 -0300

    I added border to the onClick?.

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -6,6 +6,6 @@ export const Button = (props) => {
     const { theme, setTheme } = useContext(ThemeContext)
 
     return(
-        <button onClick={()=> {setTheme(theme === themes.light ? themes.dark : themes.light)}} {...props} style={{color: theme.color, backgroundColor: theme.backgroundColor}} />
+        <button onClick={()=> {setTheme(theme === themes.light ? themes.dark : themes.light)}} {...props} style={{color: theme.color, backgroundColor: theme.backgroundColor, border: theme.border}} />
     )
 }
