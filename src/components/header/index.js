affec22f2e12e6711f584b3b63118c0056bf91ee
Moiseis Cruz <moizzito@gmail.com>
2023-11-08 11:50:11 -0300

    I created style for the header.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,31 @@
+import styled from "styled-components"
 import { Button } from "../button"
 import ImgLogo from "../imgs/rick-and-morty.png"
 
 export const Header = () => {
     return(
-        <header>
-            <img src={ImgLogo} />
+        <HeaderRAM>
+            <Logo src={ImgLogo} />
 
             <Button>
                 Mudar Tema
             </Button>
-        </header>
+        </HeaderRAM>
     )
 }
+
+const HeaderRAM = styled.header`
+    width: 100%;
+    max-width: 1440px;
+    height: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 30px;
+    margin: auto;
+`
+
+const Logo = styled.img`
+    width: 200px;
+    height: auto;
+`
