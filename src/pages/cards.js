e0c51e13b085d7fbd0b7d70b417998635dfe1c8f
Moiseis Cruz <moizzito@gmail.com>
2023-11-09 18:55:15 -0300

    I created the CardsList component code

---
+++ src/pages/cards.js
@@ -0,0 +1,11 @@
+import { Header } from "../components/header"
+import { Main } from "../components/main/main"
+
+export const CardsList = () => {
+    return(
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
