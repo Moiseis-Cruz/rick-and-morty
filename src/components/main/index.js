d4149021e29cb2a74f571f4369c04b0d393e0c7c
Moiseis Cruz <moizzito@gmail.com>
2023-11-13 18:22:01 -0300

    I changed file and component.

---
+++ src/components/main/index.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useContext } from "react";
+import styled from "styled-components";
+import { ThemeContext } from "../../contexts";
+import { Link } from "react-router-dom";
+
+async function getDatos() {
+    const response = await fetch("https://rickandmortyapi.com/api/character")
+    const datos = await response.json()
+    return datos.results
+}
+
+export const Main = () => {
+
+    const { theme } = useContext(ThemeContext)
+
+    const [ characters, setCharacters ] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getDatos()
+
+            setCharacters(data)
+        }
+        fetchData()
+    },[])
+
+    return(
+        <SectionMain style={{color: theme.color, backgroundColor: theme.backgroundColor}}>
+            <ul>
+                {
+                    characters.map((item, index) => {
+                        return(
+                            <li key={index}>
+                                <Link style={{color: theme.color, backgroundColor: theme.backgroundColor}} to={`/card/${item.id}`}>
+                                    <div>
+                                        <img src={item.image} />
+                                        <h2>{item.name}</h2>
+                                    </div>
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </SectionMain>
+    )
+}
+
+const SectionMain = styled.section`
+    width: 100%;
+    max-width: 1440px;
+    padding-top: 30px;
+    margin: auto;
+`
