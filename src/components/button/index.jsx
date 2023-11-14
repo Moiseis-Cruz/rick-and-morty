6556587da39ba6c7c10d1aa2d1252e60d12c590f
Moiseis Cruz <moizzito@gmail.com>
2023-11-14 18:37:39 -0300

    I styled BtnTogglerThemes.

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,11 +1,23 @@
 import { useContext } from "react"
 import { ThemeContext, themes } from "../../contexts"
+import styled from "styled-components"
 
 export const Button = (props) => {
 
     const { theme, setTheme } = useContext(ThemeContext)
 
     return(
-        <button onClick={()=> {setTheme(theme === themes.light ? themes.dark : themes.light)}} {...props} style={{color: theme.color, backgroundColor: theme.backgroundColor, border: theme.border}} />
+        <BtnTogglerThemes onClick={()=> {setTheme(theme === themes.light ? themes.dark : themes.light)}} {...props} style={{color: theme.color, backgroundColor: theme.backgroundColor}} />
     )
 }
+
+const BtnTogglerThemes = styled.a`
+    font-size: 30px;
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.2);
+    };&:active{
+        transform: scale(1);
+        opacity: 0.8;
+    }
+`
