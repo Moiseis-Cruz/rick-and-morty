4c3e4be7bfe86662b002cbaca90e014cab589ce6
Moiseis Cruz <moizzito@gmail.com>
2023-11-16 11:25:30 -0300

    Continuing Main stylization.

--- src/components/main/styles.js
+++ src/components/main/styles.js
@@ -1,5 +1,27 @@
 import styled from "styled-components";
 
+export const ListCharacters = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    align-items: center;
+    justify-content: center;
+`
+
+export const ContainerBtns = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+export const BtnPage = styled.button`
+    width: 180px;
+    height: 40px;
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
 export const SectionMain = styled.section`
 width: 100%;
 max-width: 1440px;
