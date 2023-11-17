090f7bac90e2ef31c7d65589c66302a9b0fb3bc9
Moiseis Cruz <moizzito@gmail.com>
2023-11-17 18:05:22 -0300

    I styled ContainerCard and CardCharater

--- src/components/card/styles.js
+++ src/components/card/styles.js
@@ -4,4 +4,21 @@ export const SectionCard = styled.section`
     width: 100%;
     max-width: 1440px;
     margin: auto;
+    height: calc(100vh - 120px);
+`
+
+export const ContainerCard = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+export const CardCharater = styled.div`
+    width: 600px;
+    display: flex;
+    gap: 10px;
+    background-color: red;
 `
