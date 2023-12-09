7a8d0f352b3ff6dc5cb92132cb9a806a6e717b19
Moiseis Cruz <moizzito@gmail.com>
2023-12-09 12:44:21 -0300

    I created the function outside of Btn Toggle Themes

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -6,8 +6,10 @@ export const Button = (props) => {
 
     const { theme, setTheme } = useContext(ThemeContext)
 
+    const handleTheme = ()=> {setTheme(theme === themes.light ? themes.dark : themes.light)}
+
     return(
-        <BtnTogglerThemes onClick={()=> {setTheme(theme === themes.light ? themes.dark : themes.light)}} {...props} theme={theme} />
+        <BtnTogglerThemes onClick={handleTheme} {...props} theme={theme} />
     )
 }
 
