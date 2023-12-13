ecdf3597480c70730f7902b22baf3a3f3d701a28
Moiseis Cruz <moizzito@gmail.com>
2023-12-13 12:39:08 -0300

    I put try & catch inside the API fetch.

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,19 @@
+const controller = new AbortController()
+
 async function getDatos(page) {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
-    const datos = await response.json()
-    return datos.results
+    try {
+        const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+        const dados = await response.json()
+        return dados
+    } catch (error) {
+        if(error.response?.status === 404 || error.message === "Network Error") {
+            controller.abort();
+        } else {
+            console.error("Erro não tratado:", error);
+            throw error;
+        }
+        
+    }
 }
 
 async function getData(id) {
