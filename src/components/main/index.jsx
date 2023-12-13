620a396db35361f2ee03848108f5d686b00c0ea4
Moiseis Cruz <moizzito@gmail.com>
2023-12-13 12:40:10 -0300

    I removed the disable states and functions.

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -2,44 +2,21 @@ import React, { useState, useEffect, useContext } from "react";
 import { ThemeContext } from "../../contexts";
 import { SectionMain, ListCharacters, LinkCards, Cards, TitleCards, ContainerBtns, BtnPage } from "./styles";
 import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
-import { getDatos } from "../../services";
+// import { getDatos } from "../../services";
 
 export const Main = () => {
 
-    const { theme } = useContext(ThemeContext)
-
-    const [ characters, setCharacters ] = useState([])
-    const [ switchPages, setSwitchPages ] = useState(1)
+    const { theme, characters, fetchData, page, hasNextPage } = useContext(ThemeContext)
 
     useEffect(() => {
-        const fetchData = async () => {
-            try{
-                const data = await getDatos(switchPages)
-                
-                setCharacters(data)
-            }catch(error){
-                console.error("Error feching data", error)
-            }
-        }
-
         fetchData()
-    },[switchPages])
-
-    const switchBetweenPages = () => {
-        setSwitchPages((prevPage => prevPage + 1))
-    }
-
-    const previousPage = () => {
-        if(switchPages > 1){
-            setSwitchPages((prevPage => prevPage - 1))
-        }
-    }
+    },[])
 
     return(
         <SectionMain theme={theme}>
             <ListCharacters>
                 {
-                    characters.map((item, index) => {
+                    characters?.map((item, index) => {
                         return(
                             <li key={index}>
                                 <LinkCards theme={theme} to={`/card/${item.id}`}>
@@ -54,8 +31,13 @@ export const Main = () => {
                 }
             </ListCharacters>
             <ContainerBtns>
-                <BtnPage onClick={previousPage} theme={theme} disabled={switchPages === 1} ><GrCaretPrevious /> Previous Page</BtnPage>
-                <BtnPage onClick={switchBetweenPages} theme={theme}>Next Page <GrCaretNext /> </BtnPage>
+                <BtnPage onClick={() => {
+                    window.location.replace(`/?page=${page - 1}`)
+                }}
+                theme={theme} disabled={page === 1}><GrCaretPrevious /> Previous Page</BtnPage>
+                <BtnPage onClick={() => {
+                    window.location.replace(`/?page=${page + 1}`)
+                }} theme={theme} disabled={!hasNextPage}>Next Page <GrCaretNext /> </BtnPage>
             </ContainerBtns>
         </SectionMain>
     )
