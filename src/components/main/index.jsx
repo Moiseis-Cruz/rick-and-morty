daaa4a4da87f31b4ea3e96819e0ea51b1755ccab
Moiseis Cruz <moizzito@gmail.com>
2024-02-05 12:39:26 -0300

    Hook not used.

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { ThemeContext } from "../../contexts";
 import { SectionMain, ListCharacters, LinkCards, Cards, TitleCards, ContainerBtns, BtnPage } from "./styles";
 import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
