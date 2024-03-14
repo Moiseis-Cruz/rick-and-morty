b447b33697c79075baefc8eabeba72181fc9aa60
Moiseis Cruz <moizzito@gmail.com>
2024-03-14 12:40:40 -0300

    I changed the location.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { ThemeProvider } from './contexts';
 import { AppRoutes } from './pages/routes';
-import { GlobalStyles } from './Styles/GlobalStyles';
+
 
 function App() {
   return (
     <>
-      <GlobalStyles />
       <ThemeProvider>
         <AppRoutes />
       </ThemeProvider>
