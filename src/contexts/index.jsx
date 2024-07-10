9fb8458c181e47ef914af8a3d098a27e8348c550
Moiseis Cruz <moizzito@gmail.com>
2024-07-09 21:16:47 -0300

    I added sessionStorage.

--- src/contexts/index.jsx
+++ src/contexts/index.jsx
@@ -25,7 +25,7 @@ export const ThemeContext = createContext({});
 
 export const ThemeProvider = (props) => {
 
-    const savedTheme = localStorage.getItem("themes");
+    const savedTheme = sessionStorage.getItem("themes");
 
     const [ theme, setTheme ] = useState(savedTheme ? JSON.parse(savedTheme) : themes.light);
 
